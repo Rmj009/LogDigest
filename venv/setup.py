2177f9e69bb99b5bb623007695bf47221bf4d149
kouki_liu <r05546043@ntu.edu.tw>
2023-11-28 23:45:13 +0800

    add Represent for plot diagram, test for pytest

--- venv/setup.py
+++ venv/setup.py
@@ -4,7 +4,7 @@ from .lib import *
 setup(
     name='my_package',
     version='1.0',
-    packages=find_packages(),
+    # packages=find_packages(),
     install_requires=[
         # List your dependencies here
     ],
