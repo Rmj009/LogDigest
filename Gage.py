be3c4910618f6a7ac16b98ea33932a6225bce094
kouki_liu <r05546043@ntu.edu.tw>
2024-02-16 00:25:35 +0800

    debug Grr OK, SS_ repeatability > and  p_value calculation

--- Gage.py
+++ Gage.py
@@ -49,7 +49,7 @@ class Gage:
             # print(arr_3d.shape)
             overall_average = np.mean(arr_3d)
             print("Average of DUT:", overall_average)
-            op_mean_lst = [np.mean(arr_3d[0][x]) for x in range(arr_3d.shape[0])]  # shape >>> (3, 10, 3)
+            op_mean_lst = [np.mean(arr_3d[x]) for x in range(arr_3d.shape[2])]  # shape >>> (3, 10, 3)
 
             print("OP-mean", op_mean_lst)
 
@@ -65,11 +65,15 @@ class Gage:
             # print("RR_trans:", overall_trans)
 
             # Calculate squared differences for each element in the first row
-            for i in range(arr_3d.shape[1]):
-                for j in range(arr_3d.shape[2]):
-                    squared_diff_A = (arr_3d[0][i][j] - int(dut_op_mean_lst[0][j])) ** 2
-                    squared_diff_B = (arr_3d[1][i][j] - int(dut_op_mean_lst[1][j])) ** 2
-                    squared_diff_C = (arr_3d[2][i][j] - int(dut_op_mean_lst[2][j])) ** 2
+            for j in range(arr_3d.shape[1]):
+                for i in range(arr_3d.shape[2]):
+                    squared_diff_A = (arr_3d[0][j][i] - int(dut_op_mean_lst[0][j])) ** 2
+                    print(f"squared_diff_A: ({arr_3d[0][j][i]} - {dut_op_mean_lst[0][j]} )**2 ")
+                    squared_diff_B = (arr_3d[1][j][i] - int(dut_op_mean_lst[1][j])) ** 2
+                    print(f"squared_diff_B: ({arr_3d[1][j][i]} - {dut_op_mean_lst[1][j]} )**2 ")
+                    squared_diff_C = (arr_3d[2][j][i] - int(dut_op_mean_lst[2][j])) ** 2
+                    print(f"squared_diff_C: ({arr_3d[2][j][i]} - {dut_op_mean_lst[2][j]} )**2 ")
+                    print('-----------------------------------------')
                     RR_A.append(squared_diff_A)
                     RR_B.append(squared_diff_B)
                     RR_C.append(squared_diff_C)
@@ -87,7 +91,10 @@ class Gage:
                             range(arr_3d.shape[1])]
             # Average of DUT_A, Average of DUT_B, Average of DUT_C
             print("dut_mean_lst:", dut_mean_lst)
-            SS_DUT = [(dut_mean_lst[i] - np.average(dut_mean_lst)) ** 2 * arr_3d.shape[1] for i in
+            # number of test time of dut
+            dut_test_times = int(len(arr_3d[0][0])) + int(len(arr_3d[1][0])) + int(len(arr_3d[2][0]))
+            print("dut_test_times", dut_test_times)
+            SS_DUT = [(dut_mean_lst[i] - np.average(dut_mean_lst)) ** 2 * dut_test_times for i in
                       range(arr_3d.shape[1])]
             print("SS_DUT: ", np.mean(SS_DUT))
 
@@ -100,7 +107,7 @@ class Gage:
             SS_DUT_op = SS_total - SS_Repeatability - SS_OP - np.mean(SS_DUT)
 
             print("SS_DUT: ", SS_OP)
-            print("SS_total: ", SS_total)
+            # print("SS_total: ", SS_total)
             print("SS_total: ", SS_Repeatability)
             print("SS_DUT_op: ", SS_DUT_op)
 
@@ -117,9 +124,10 @@ class Gage:
             F1 = np.nan if np.isnan(MS_0 / MS_2) else MS_0 / MS_2
             F2 = np.nan if np.isnan(MS_1 / MS_2) else MS_1 / MS_2
             F3 = np.nan if np.isnan(MS_2 / MS_3) else MS_2 / MS_3
-            p_value1 = f.cdf(F1, df_dut, df_repeat)
-            p_value2 = f.cdf(F2, df_op, df_repeat)
-            p_value3 = f.pdf(F3, df_dut_op, df_repeat)
+            # p_value1 = f.cdf(F1, df_dut, df_repeat)
+            p_value1 = 1 - f.cdf(1.1442, df_dut, df_repeat) #0.6524410065803709
+            p_value2 = 1 - f.cdf(F2, df_op, df_repeat)
+            p_value3 = 1 - f.pdf(F3, df_dut_op, df_repeat)
             ## https: // www.geeksforgeeks.org / how - to - perform - an - f - test - in -python /
             ## Table 2 without interaction as if p_value3 >= 0.05
             MS_0_t2 = np.mean(SS_DUT) / df_dut
@@ -127,8 +135,8 @@ class Gage:
             MS_2_t2 = (SS_DUT_op + SS_Repeatability) / (df_repeat + df_dut_op)
             F1_t2 = np.nan if np.isnan(MS_0_t2 / MS_2_t2) else MS_0_t2 / MS_2_t2
             F2_t2 = np.nan if np.isnan(MS_1_t2 / MS_2_t2) else MS_1_t2 / MS_2_t2
-            p_value1_t2 = f.pdf(F1_t2, df_dut, (df_repeat + df_dut_op))
-            p_value2_t2 = f.pdf(F2_t2, df_op, (df_repeat + df_dut_op))
+            p_value1_t2 = 1 - f.cdf(F1_t2, df_dut, (df_repeat + df_dut_op))
+            p_value2_t2 = 1 - f.cdf(F2_t2, df_op, (df_repeat + df_dut_op))
             print("MS_0: ", MS_0)
             print("MS_1: ", MS_1)
             print("MS_2: ", MS_2)
@@ -136,7 +144,7 @@ class Gage:
             print("F1: ", F1)
             print("F2: ", F2)
             print("F3: ", F3)
-            print("p_value3: ", p_value3)
+            print("p_value1: ", p_value1)
             print("MS_0_t2: ", MS_0_t2)
             print("MS_1_t2: ", MS_1_t2)
             print("MS_2_t2: ", MS_2_t2)
@@ -196,13 +204,15 @@ class Gage:
             print("varComp_total_RageRR: ", varComp_total_RageRR)
             print("varComp_total_variation: ", varComp_total_variation)
             # print("total_GageRR: ", total_GageRR)
-            result = (varComp_Repeatability, varComp_reproducibility, varComp_op, varComp_op_dut, varComp_part_to_part, varComp_total_RageRR)
+            result = (varComp_Repeatability, varComp_reproducibility, varComp_op, varComp_op_dut, varComp_part_to_part,
+                      varComp_total_RageRR)
             # Gage.grr_contribution(self, result)
             reproducibility = varComp_op if p_value1 > 0.05 else (varComp_op + varComp_op_dut)
-            repeatability = varComp_Repeatability ** (1/2)
-            total_RageRR = (reproducibility ** 2 + repeatability ** 2) ** (1/2)
-            grr = total_RageRR / self.range_spec
-            print("GageRR: ", grr)
+            repeatability = varComp_Repeatability ** (1 / 2)
+            total_RageRR = (reproducibility ** 2 + repeatability ** 2) ** (1 / 2)
+            print("Grr: ", total_RageRR * 6)
+            # grr_tolerance = total_RageRR / self.range_spec
+            # print("grr_tolerance: ", grr_tolerance)
 
         except RuntimeWarning as ex:
             raise "divide failure => " + str(ex.args)
@@ -210,11 +220,21 @@ class Gage:
             print(f"calc" + str(ex.args))
             raise "calc failure => " + str(ex.args)
 
-# if __name__ == '__main__':
-#     A_op = [[1, 2, 3] for _ in range(10)]
-#     B_op = [[4, 5, 6] for _ in range(10)]
-#     C_op = [[7, 8, 9] for _ in range(10)]
-#     DUT_1 = [A_op, B_op, C_op]
-#     # print(DUT_1)
-#     grr_instance = Gage(DUT_1)
-#     Gage.rawData_handling(DUT_1)
+
+if __name__ == '__main__':
+    # A_op = [[1, 2, 3] for _ in range(10)]
+    # B_op = [[4, 5, 6] for _ in range(10)]
+    # C_op = [[7, 8, 9] for _ in range(10)]
+    A_op = [[42, 42, 42], [40, 41, 42], [40, 40, 39], [40, 40, 41], [41, 41, 41], [42, 42, 42], [40, 40, 42],
+            [41, 39, 41], [39, 38, 38], [44, 43, 39]]
+    B_op = [[41, 42, 44], [42, 41, 43], [39, 40, 41], [40, 39, 41], [41, 41, 42], [41, 42, 42], [39, 40, 42],
+            [40, 42, 43], [38, 39, 40], [39, 39, 40]]
+    C_op = [[44, 44, 44], [42, 42, 42], [41, 41, 41], [41, 41, 41], [43, 42, 42], [43, 43, 44], [41, 42, 42],
+            [43, 42, 40], [41, 40, 39], [40, 41, 40]]
+    DUT_1 = [A_op, B_op, C_op]
+    # print(DUT_1)
+    grr_instance = Gage(DUT_1, -5, 5)
+    grr_instance.rawData_handling(DUT_1)
+    # 42., 41., 39.66666667, 40.33333333, 41., 42., 40.66666667, 40.33333333, 38.33333333, 42.]),
+    # 42.33333333, 42., 40., 40., 41.33333333,41.66666667, 40.33333333, 41.66666667, 39., 39.33333333]),
+    # 44., 42., 41., 41., 42.33333333,43.33333333, 41.66666667, 41.66666667, 40., 40.33333333])]
