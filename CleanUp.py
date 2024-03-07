f6091fcfa48721a630492426edda1d4ba4997990
kouki_liu <r05546043@ntu.edu.tw>
2024-03-08 01:45:23 +0800

    parsing LOG spec, values ok

--- CleanUp.py
+++ CleanUp.py
@@ -1,10 +1,11 @@
+import numpy as np
 import pandas as pd
 
 
 class CleanUp(object):
-    global spec_range
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, spec_range):
+        self.spec_range = spec_range
         self.file_path = file_path
 
     # def gather_lines_containing_string(directory_path, string_to_find, string_to_find2, output_csv_path):
@@ -33,40 +34,57 @@ class CleanUp(object):
     #     except UnicodeDecodeError:
     #                     print(f"Error decoding file {filename}. Skipping...")
 
+    @staticmethod
+    def washing_spec(self, *args):
+        spec_lst = args
+        try:
+            self.spec_range = spec_lst if self.spec_range is None else None
+
+        except Exception as e:
+            raise "washing_spec$NG >>> " + str(e.args)
+
     def washing(self):
+        """
+        Diagnosis >>> count test items
+        :return:
+        """
         # d_path = "C:\Users\23002496\PycharmProjects\DigestATSuite\IMQX.csv"
         keyword1 = "SPEC:"
         keyword2 = "Value:"
+        specRange = ""
+        testItem_values = ""
+        all_lst = []
         spec_lst = []
-        spec_lst2 = []
+        data_lst = []
         values_lst = []
-        values_lst2 = []
+        countTestItems = 5810
+        IsSpecRange_ready = False
+        # countStressTimes = 10  # num_lst = 0
         try:
             with open(self.file_path, mode='r', encoding='utf-16', errors='ignore') as file:
                 # Create a CSV reader object
                 lines = file.readlines()
                 for line in lines:
-                    # Find the indices of the keywords in the line
-                    index1 = line.find(keyword1)
-                    index2 = line.find(keyword2)
-                    index3 = line.find(keyword2, index1 + len(keyword1))
-
-                    # Extract the substring between the keywords if both keywords are found
-                    if index1 != -1 and index2 != -1:
-                        spec_range = line[index1 + len(keyword1):index2].strip()
-                        spec_values = line[index2 + len(keyword2):].strip()
-                        if len(spec_lst) > 10000:
-                            spec_lst2.append(spec_range)
-                            values_lst2.append(spec_values)
-                        spec_lst.append(spec_range)
-                        values_lst.append(spec_values)
-                s = pd.Series(spec_lst)
-                v = pd.Series(values_lst)
-                s2 = pd.Series(spec_lst2)
-                v2 = pd.Series(values_lst2)
-                df = pd.DataFrame([s, s2, v, v2])
+                    if len(data_lst) < countTestItems:
+                        # Find the indices of the keywords in the line
+                        index1 = line.find(keyword1)
+                        index2 = line.find(keyword2)
+                        # index3 = line.find(keyword2, index1 + len(keyword1))
+                        if index1 != -1 and index2 != -1:
+                            specRange = line[index1 + len(keyword1):index2].strip()
+                            testItem_values = line[index2 + len(keyword2):].strip()
+                            data_lst.append(testItem_values)
+                            if not IsSpecRange_ready:
+                                spec_lst.append(specRange)
+                    else:
+                        # acquire spec_lst once
+                        IsSpecRange_ready = True
+                        all_lst.append(data_lst)
+                        data_lst = []
+                df = pd.DataFrame(np.array(all_lst))
                 df = df.T
-                df.to_csv("iiii.csv", sep=',')
+                df['spec'] = pd.Series(spec_lst)
+                df.to_csv("IMQX_sample.csv", sep=',')
             file.close()
 
         except Exception as e:
@@ -75,9 +93,9 @@ class CleanUp(object):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # filename = input("csv name")
+    # filename = input("csv name: \r\n")
     filename = 'C:\\Users\\23002496\\PycharmProjects\\DigestATSuite\\IMQX.txt'
-    ats_instance = CleanUp(filename)
+    ats_instance = CleanUp(filename, None)
     ats_instance.washing()
     # directory_path = "D:\DataSystem_NPI_v2.0.4_20230421\ATS_Log\Breakdown"
     # directory_path = "D:\Analysis tool 20230321\ParseLog\IMQX"
@@ -86,7 +104,3 @@ if __name__ == '__main__':
     # output_csv_path = "IMQX.csv"
 
     # gather_lines_containing_string(directory_path, string_to_find, string_to_find2, output_csv_path)
-
-    # ----------------------------------------------------------
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
